4da91e32b074ea73be79f3f2366ca003196b653d
shiv <shiv.sr@ibm.com>
2024-02-24 20:47:38 -0500

    adding support for ibm labradorite

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -2258,6 +2258,17 @@ class YuanAdapter(BaseModelAdapter):
         return get_conv_template("yuan")
 
 
+class LabradoriteAdapter(BaseModelAdapter):
+    """The model adapter for ibm/labradorite-13b"""
+
+    def match(self, model_path: str):
+        return "labradorite" in model_path.lower()
+    
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("labradorite")
+
+
+
 # Note: the registration order matters.
 # The one registered earlier has a higher matching priority.
 register_model_adapter(PeftModelAdapter)
@@ -2347,6 +2358,7 @@ register_model_adapter(SolarAdapter)
 register_model_adapter(SteerLMAdapter)
 register_model_adapter(LlavaAdapter)
 register_model_adapter(YuanAdapter)
+register_model_adapter(LabradoriteAdapter)
 
 # After all adapters, try the default base adapter.
 register_model_adapter(BaseModelAdapter)
