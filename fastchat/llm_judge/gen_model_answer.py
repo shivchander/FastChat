98d7bbaec41a98edc411b5ae4d5c7c76be06e15f
shiv <shiv.sr@ibm.com>
2024-03-01 22:05:23 -0500

    removing rep

--- fastchat/llm_judge/gen_model_answer.py
+++ fastchat/llm_judge/gen_model_answer.py
@@ -126,7 +126,6 @@ def get_model_answers(
                         do_sample=do_sample,
                         temperature=temperature,
                         max_new_tokens=max_new_token,
-                        repetition_penalty=1.05,
                     )
                     if model.config.is_encoder_decoder:
                         output_ids = output_ids[0]
